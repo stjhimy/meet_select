01f9047897a7618d46a3d443be16b6fbbafd7e9b
stjhimy <stjhimy@gmail.com>
2023-02-07 12:52:50 -0300

    Update API glossary / add documentation

--- lib/meet_select/glossary.ex
+++ lib/meet_select/glossary.ex
@@ -1,6 +1,6 @@
 defmodule MeetSelect.Glossary do
   @api [
-    authenticate: %{
+    auth_api_token: %{
       url: "/auth/api/token/",
       method: :post,
       doc: """
@@ -24,7 +24,7 @@ defmodule MeetSelect.Glossary do
         password: {:string, :required}
       ]
     },
-    search_hotels: %{
+    hotel_search_list: %{
       url: "/hotel/search/list/",
       method: :get,
       doc: """
@@ -34,8 +34,16 @@ defmodule MeetSelect.Glossary do
       - Optional keys: []
 
       ### Examples:
-          iex> MeetSelect.list_hotels(check_in: "", check_out: "")
-          {200, %{}}
+          iex> params = [
+                check_in: "2023-12-01",
+                check_out: "2023-12-02",
+                rooms: 1,
+                adults: 1,
+                lat: "",
+                long: ""
+          ]
+          iex> MeetSelect.list_hotels(params)
+          {200, [%{id: "foo", name: "bar"}]}
       """,
       params: [
         check_in: {:string, :required},
@@ -45,42 +53,78 @@ defmodule MeetSelect.Glossary do
         latitude: {:string, :required},
         longitude: {:string, :required}
       ]
+    },
+    hotel_search_detail: %{
+      url: "/hotel/search/detail/",
+      method: :get,
+      doc: """
+      Get details for a specific hotel.
+
+      - Required keys: [:hotel_id, :check_in, :check_out, :rooms]
+      - Optional keys: []
+
+      ### Examples
+          iex> MeetSelect.hotel_search_detail(hotel_id: "hotel_id", check_in: "2023-12-01", check_out: "2023-12-02", rooms: 1)
+          {200, %{}}
+      """,
+      params: [
+        hotel_id: {:integer, :required},
+        rooms: {:integer, :required},
+        check_in: {:string, :required},
+        check_out: {:string, :required}
+      ]
+    },
+    hotel_location: %{
+      url: "/hotel/location/",
+      method: :get,
+      doc: """
+      Retrieve hotel locations.
+
+      - Required keys: [:search]
+      - Optional keys: []
+
+      ### Examples
+          iex> MeetSelect.hotel_location(search: "usa")
+          {200, %{}}
+      """,
+      params: [
+        search: {:string, :required}
+      ]
+    },
+    hotel_detail: %{
+      url: "/hotel/:id/detail/",
+      method: :get,
+      doc: """
+      Get details for a specific hotel.
+
+      - Required keys: [id]
+      - Optional keys: []
+
+      ### Examples
+          iex> MeetSelect.hotel_details(id: "hotel_id")
+          {200, %{}}
+      """,
+      params: [
+        id: {:string, :required}
+      ]
+    },
+    hotel_review: %{
+      url: "/hotel/:id/review/",
+      method: :get,
+      doc: """
+      Get reviews for a specific hotel.
+
+      - Required keys: [id]
+      - Optional keys: []
+
+      ### Examples
+          iex> MeetSelect.hotel_review(id: "hotel_id")
+          {200, %{}}
+      """,
+      params: [
+        id: {:string, :required}
+      ]
     }
-    # search_hotel_details: %{
-    #   url: "/hotel/search/detail/",
-    #   method: :get,
-    #   docs: "",
-    #   params: [
-    #     hotel_id: {:integer, :required},
-    #     rooms: {:integer, :required},
-    #     check_in: {:string, :required},
-    #     check_out: {:string, :required}
-    #   ]
-    # },
-    # search_locations: %{
-    #   url: "/hotel/location/",
-    #   method: :get,
-    #   docs: "",
-    #   params: [
-    #     search: {:string, :required}
-    #   ]
-    # },
-    # get_hotel_details: %{
-    #   url: "/hotel/:id/detail/",
-    #   method: :get,
-    #   docs: "",
-    #   params: [
-    #     id: {:string, :required}
-    #   ]
-    # },
-    # get_hotel_reviews: %{
-    #   url: "/hotel/:id/review/",
-    #   method: :get,
-    #   docs: "",
-    #   params: [
-    #     id: {:string, :required}
-    #   ]
-    # }
   ]
 
   @spec glossary() :: any()
